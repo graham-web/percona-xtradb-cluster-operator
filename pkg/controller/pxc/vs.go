3113a54fac18611f7178e277da5442ec8d1c20f0
qJkee <zamalutdinovbulat@gmail.com>
2020-07-09 17:33:08 +0300

    final version of version service

--- pkg/controller/pxc/vs.go
+++ pkg/controller/pxc/vs.go
@@ -104,7 +104,7 @@ type DepVersion struct {
 }
 
 type VersionService interface {
-	GetExactVersion(versionMeta versionMeta) (DepVersion, error)
+	GetExactVersion(vm versionMeta) (DepVersion, error)
 }
 
 type VersionServiceClient struct {
